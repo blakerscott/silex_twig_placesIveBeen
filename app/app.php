a211488fa1e58e2294d3518af637aa97f217fabe
Brianna Popson and Blake Scott <blakerscott@gmail.com>
2016-02-10 09:43:54 -0800

    create inital twig files

--- app/app.php
+++ app/app.php
@@ -1,34 +1,38 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/City.php";
+
+    session_start();
+
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "<!DOCTYPE html>
-        <html>
-        <head>
-            <title>Hello Friend!</title>
-            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
-        </head>
-        <body>
-            <div class='container'>
-                <h1>Hello From Afar</h1>
-                <p>Dear Friend,</p>
-                <p>How are you? I hope that you are having a nice weekend. I'm vacationing in the mountains of Tibet while I learn programming! </p>
-                <p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>
-                <p>But I like programming a lot, so I've got that going for me. </p>
-                <p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>
-                <p>Cheers,</p>
-                <p>Travel Enthusiast Jane</p>
-                <p><a href='/goodbye'>goodbye!</a></p>
-            </div>
-        </body>
-        </html>";
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+
+
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
-    $app->get("/goodbye", function() {
-        return "Goodbye friend!
-        <p><a href='/'>hello punk!</a> </p>";
+
+    $app->post("/tasks", function() use ($app) {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
     });
+
+    $app->post("/delete_tasks", function() use ($app) {
+        Task::deleteAll();
+        return $app['twig']->render('delete_tasks.html.twig');
+    });
+
+
 
     return $app;
 ?>
